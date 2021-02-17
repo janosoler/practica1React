cc1ad13bd40f22d533c7086408c79d67ed51512e
Jano Soler <janosolerdiaz@gmail.com>
2021-02-18 00:24:11 +0100

    hecho hasta el punto 5

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = (props) => {
+    return (
+        <div>
+        <p>{props.username}Lorem ipsum dolor sit amet, consectetur adipiscing elit. In porta lectus nec lorem cursus, a pellentesque erat suscipit. Aliquam at magna urna. Suspendisse lobortis a ligula id luctus. Nulla volutpat tortor justo, in vehicula tellus laoreet vel. Vestibulum et massa gravida mauris feugiat posuere. Sed tempor consectetur orci sit amet placerat. Nam eu urna id erat maximus vestibulum eu non risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam et felis egestas, scelerisque nibh rhoncus, egestas diam. Nulla suscipit turpis at enim tempor commodo. Maecenas maximus pulvinar finibus. Fusce eget blandit massa.</p>
+        <p>Aliquam scelerisque pellentesque elementum. Sed tincidunt turpis leo, sit amet rutrum dui finibus id. Duis vestibulum purus ac sem vehicula, nec efficitur sapien condimentum. Etiam quis commodo tellus. Integer posuere dui sapien, sit amet finibus ipsum pretium nec. Aliquam nulla orci, ornare nec turpis eu, pellentesque consectetur nibh. Sed luctus feugiat rutrum. Nullam vehicula nisi tellus, at tristique augue viverra sed. Vestibulum pulvinar nulla non tempor rhoncus. Morbi et lacus nec dui rhoncus euismod non eget purus. Phasellus accumsan massa non est lobortis, non dapibus tellus tincidunt. Suspendisse tempus tellus eros, et pharetra arcu pretium sit amet. Duis tincidunt bibendum quam.</p>
+        </div>
+    )
+}
+
+export default userOutput;
